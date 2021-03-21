c8786c8737eae3b3e80031edce25005b15b7f132
bhaveshkumar80 <bhavesh8094148735@gmail.com>
2021-03-21 17:46:57 +0530

    first commit

---
+++ model.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+import pickle
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv('hiring.csv')
+
+df['experience'].fillna(0, inplace=True)
+
+df['test_score'].fillna(df['test_score'].mean(), inplace=True)
+
+X = df.iloc[:, :3]
+
+def convert_to_int(word):
+    word_dict = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
+                'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, 'ten': 10,
+                'eleven': 11, 'twelve': 12, 'zero': 0, 0: 0}
+    return word_dict[word]
+
+X['experience'] = X['experience'].apply(lambda x: convert_to_int(x))
+
+y = df.iloc[:, -1]
+
+lr = LinearRegression()
+
+lr.fit(X, y)
+
+pickle.dump(lr, open('model.pkl', 'wb'))
+
+model = pickle.load(open('model.pkl', 'rb'))
+
+print(model.predict([[2, 9, 6]]))
+
